2fe3682cb60b5534e39b3cf6cd100e3856974101
Myadam <sai.kiran.myadam@accenture.com>
2026-05-19 00:03:25 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+# 1. Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = "rg-terraform-cicd-demo"
+  location = "East US 2"
+  
+  tags = {
+    project     = "terraform-cicd"
+    environment = "dev"
+    managed-by  = "terraform"
+  }
+}
+
+# 2. Storage Account
+resource "azurerm_storage_account" "main" {
+  name                     = "saikirantfcicddemo"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+  tags = {
+    project     = "terraform-cicd"
+    environment = "dev"
+  }
+}
+
+# 3. Storage Container
+resource "azurerm_storage_container" "main" {
+  name                  = "democontainer"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "saikiranterraformcicd"
+    container_name       = "terraformcontainer"
+    key                  = "terraform-cicd.tfstate"
+  }
+}
+
+
+
+
+provider "azurerm" {
+  features {}
+}
